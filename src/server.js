230b8e1cb74eb5e6611486343f1408fe6295627c
Bikram Roy <bikramroy364@gmail.com>
2024-07-17 21:01:56 +0530

    fix build error

--- src/server.js
+++ src/server.js
@@ -25,16 +25,16 @@ app.use("/api/user", userRouters);
 app.use("/api/message", messageRouters);
 
 //------------ Deployment-------------//
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-  });
-} else {
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/build")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+//   });
+// } else {
   app.get("/", (req, res) => {
     res.send("Api is running");
   });
-}
+// }
 
 //------------ Deployment-------------//
 app.use(pathNotFoundRouters);
